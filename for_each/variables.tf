a5b942e5c6ac364af9ba5eeb000ba88eadf3a661
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-24 10:11:43 +0530

    terraform

--- for_each/variables.tf
+++ for_each/variables.tf
@@ -1,15 +1,23 @@
+#map
+# variable "instances" {
+#     type = map
+#     default = {
+#        mongodb = "t3.micro"
+#        redis = "t3.micro"
+#        mysql = "t3.small"
+#        rabbitmq = "t3.micro"
+#        catalogue = "t3.micro"
+#        user = "t3.micro"
+#        cart = "t3.micro"
+#        shipping = "t3.small"
+#     }
+# }
+
+#this should be converted into set
+
 variable "instances" {
-    type = map
-    default = {
-       mongodb = "t3.micro"
-       redis = "t3.micro"
-       mysql = "t3.small"
-       rabbitmq = "t3.micro"
-       catalogue = "t3.micro"
-       user = "t3.micro"
-       cart = "t3.micro"
-       shipping = "t3.small"
-    }
+    type = list
+    default = ["mongodb", "redis"]
 }
 
 variable "zone_id" {
